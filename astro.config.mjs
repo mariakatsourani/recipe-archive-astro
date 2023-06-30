b8dfe1bcbdd336f97c74e0541f36be97e88550f9
Maria Katsourani <mariakatsourani@users.noreply.github.com>
2023-06-30 23:32:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "mariakatsourani.github.io",
-  base: "/recipe-archive-astro-base",
+  site: "https://mariakatsourani.github.io",
+  base: "/recipe-archive-astro",
   integrations: [mdx(), tailwind()],
 });
